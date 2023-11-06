2306bce7e01fd5a1887948c82a54db21d06ef606
nikolozi2001 <mr.qachibaia44@gmail.com>
2023-11-06 23:06:03 +0400

    added photos and updated API,  needs fix

--- script/api.js
+++ script/api.js
@@ -1,30 +1,77 @@
-const cardContainer = document.getElementById("card-container");
+// For API data
 
-// Fetch data from the API
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((posts) => {
-    posts.forEach((post) => {
-      const card = createCard(post);
-      cardContainer.appendChild(card);
-    });
+// Create Cards for APIData
+
+const getAPIData = (userId, title, body) => {
+  // Create div for apiCards
+  const apiCard = document.createElement("div");
+  apiCard.classList.add("api_card");
+
+  // Create h1 for userId
+  const apiId = document.createElement("h1");
+  apiId.textContent = userId;
+
+  // Create h2 for title
+  const apiTitle = document.createElement("h2");
+  apiTitle.textContent = title;
+
+  // Create paragraph for body
+  const apiBody = document.createElement("p");
+  apiBody.classList.add("paragraph");
+  apiBody.textContent = body;
+
+  // append all in one div
+  apiCard.append(apiId, apiTitle, apiBody);
+
+  return apiCard;
+};
+
+// Fetch the data
+const APIData = fetch("https://jsonplaceholder.typicode.com/posts");
+
+APIData.then((apiData) => apiData.json())
+  .then((datas) => {
+    // Get the main container for cards
+    const apiContent = document.querySelector(".api_content");
+
+    const cardsPerPage = 18;
+    let start = 0;
+
+    const forLoop = () => {
+      for (let i = start; i < start + cardsPerPage && i < datas.length; i++) {
+        const cardElement = getAPIData(
+          datas[i].userId,
+          datas[i].title,
+          datas[i].body
+        );
+        apiContent.appendChild(cardElement);
+      }
+    };
+
+    forLoop();
+
+    const nextCards = () => {
+      apiContent.innerHTML = "";
+      start += cardsPerPage;
+      if (start >= datas.length) {
+        start = datas.length - 1;
+      }
+      forLoop();
+    };
+
+    const prevCards = () => {
+      apiContent.innerHTML = "";
+      start -= cardsPerPage;
+      if (start < 0) {
+        start = 0;
+      }
+      forLoop();
+    };
+
+    const nextButton = document.querySelector(".next_button");
+    nextButton.addEventListener("click", nextCards);
+
+    const prevButton = document.querySelector(".prev_button");
+    prevButton.addEventListener("click", prevCards);
   })
-  .catch((error) => {
-    console.error("Error fetching data: ", error);
-  });
-
-function createCard(post) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  const title = document.createElement("h3");
-  title.textContent = post.title;
-
-  const body = document.createElement("p");
-  body.textContent = post.body;
-
-  card.appendChild(title);
-  card.appendChild(body);
-
-  return card;
-}
+  .catch((error) => console.error(error));
